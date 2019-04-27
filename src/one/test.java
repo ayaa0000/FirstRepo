c730a26cd42a56713026881ae5ae33cfc09136f4
ayaa0000 <49100282+ayaa0000@users.noreply.github.com>
2019-04-27 16:14:29 +0200

    aya

--- src/one/test.java
+++ src/one/test.java
@@ -2,6 +2,8 @@ package one;
 
 public class test {
 	public static void main ( String arg[]) {
-		System.out.println("hello");	
+		System.out.println("hello");
+		System.out.println("aya");
+		
 	}
 }
